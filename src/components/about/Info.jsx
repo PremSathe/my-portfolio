3fe7d2e388e6bfd07805bb815385a4dcfcd1043d
PremSathe <premsathe78@gmail.com>
2024-08-29 16:12:59 +0530

    skills

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -4,21 +4,21 @@ const Info = () => {
   return (
     <div className="about__info grid">
       <div className="about__box">
-        <i className="bx bx-award"></i>
+        <i className="bx bx-award about__icon"></i>
         <h3 className="about__title">
           Experiance
           <span className="about__subtitle">1 Years Working</span>
         </h3>
       </div>
       <div className="about__box">
-        <i className="bx bx-briefcase"></i>
+        <i className="bx bx-briefcase about__icon"></i>
         <h3 className="about__title">
           Completed
           <span className="about__subtitle">15 + Projects</span>
         </h3>
       </div>
       <div className="about__box">
-        <i className="bx bx-support"></i>
+        <i className="bx bx-support about__icon"></i>
         <h3 className="about__title">
           Support
           <span className="about__subtitle">Online 24/7</span>
