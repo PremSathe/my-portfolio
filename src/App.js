a38d253da306dad16465ad405429a4b0c6ca8b80
PremSathe <premsathe78@gmail.com>
2024-09-04 16:14:09 +0530

    Contact

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import About from "./components/about/About";
+import Contact from "./components/contact/Contact";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
 import Qualification from "./components/qualification/Qualification";
 import Services from "./components/services/Services";
 import Skills from "./components/skills/Skills";
+// import Testimonial from "./components/testimonial/Testimonial";
 function App() {
   return (
     <>
@@ -15,6 +17,8 @@ function App() {
         <Skills />
         <Services />
         <Qualification />
+        {/* <Testimonial /> */}
+        <Contact />
       </main>
     </>
   );
