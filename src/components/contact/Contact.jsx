97acae7c921d28b5b82eaac49442d04f46679f87
PremSathe <premsathe78@gmail.com>
2024-09-05 14:23:12 +0530

    contact-email

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,6 +1,25 @@
-import React from "react";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 import "./contact.css";
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm("service_5lixa1w", "template_erg8btb", form.current, {
+        publicKey: "lHLs6UxHuFbl6GEYi",
+      })
+      .then(
+        () => {
+          console.log("SUCCESS!");
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+        }
+      );
+  };
   return (
     <section className="contact" id="contact">
       <h2 className="section__title">Get in touch</h2>
@@ -56,7 +75,7 @@ const Contact = () => {
         <div className="contact__content">
           <h3 className="contact__title">Write me your project</h3>
 
-          <form className="contact__form">
+          <form ref={form} onSubmit={sendEmail}>
             <div className="contact__form-div">
               <label htmlFor="" className="contact__form-tag">
                 Name
